dab52edadb5d4b0f15b738880070766c327f747a
ampuEus <ampueus@gmail.com>
2023-12-11 19:40:32 +0100

    parser iteration done

--- src/parser.c
+++ src/parser.c
@@ -1,50 +1,119 @@
 #include "minishell.h"
 #include "libft.h"
 
+// TODO delete includes
+#include <stdio.h>
+
+/*
+NOTE: Rows counter start from 1.
+*/
+static unsigned int	get_table_rows(char **tokens)
+{
+	unsigned int	tokens_len;
+	unsigned int	rows;
+
+	rows = 1;
+	tokens_len = 0;
+	while (tokens[tokens_len])
+	{
+		if (tokens[tokens_len][0] == PIPE && tokens_len != 0)
+			rows++;
+		tokens_len++;
+	}
+	return (rows);
+}
+
+/* Get the row's token number until next pipe */
+static unsigned int	get_rows_column(unsigned int actual_row, char **tokens)
+{
+	unsigned int	row;
+	unsigned int	token_nbr;
+
+	token_nbr = 0;
+	while (tokens[token_nbr])
+	{
+		if (tokens[token_nbr][0] == PIPE)
+			break;
+		token_nbr++;
+	}
+	return (token_nbr);
+}
+
+static char	fill_table_cmds(char ***table, char **tokens, unsigned int tot_rows)
+{
+	unsigned int	row;
+	unsigned int	column;
+	unsigned int	token;
+
+	row = 0;
+	column = 0;
+	token = 0;
+	table[row] = ft_calloc(get_rows_column(row, &tokens[token]) + 1, sizeof(*table));
+	while(tokens[token])
+	{
+		if (row >= tot_rows)
+			return (1, printf("PARSER ERROR: Commands table rows exceeded.\n"));
+		if (tokens[token][0] == PIPE)
+		{
+			token++;
+			if (column != 0)
+			{
+				table[row][column] = NULL;
+				row++;
+				table[row] = ft_calloc(get_rows_column(row, &tokens[token]) + 1, sizeof(*table));
+				column = 0;
+			}
+		}
+		table[row][column] = ft_strdup(tokens[token]);
+		token++;
+		column++;
+	}
+	return (0);
+}
+
 /*
 Take the tokens from lexer and return a structured table.
 
 Return example:
 +-------------+-------------+--------------+-------+-------+
-| cmd_1       | -options    | arg_1        | arg_2 |       |
-+-------------+-------------+--------------+-------+-------+
-| cmd_2       | arg_1       | arg_2        | ...   | arg_n |
-+-------------+-------------+--------------+-------+-------+
-| cmd_n       | -options    |              |       |       |
-+-------------+-------------+--------------+-------+-------+
-| In: default | Out: file_1 | Err: default |       |       |
-+-------------+-------------+--------------+-------+-------+
-*/
-char	**parser(char	**tokens)
-{
-	char			**cmds_table;
-	unsigned int	tokens_len;
-	unsigned int	pipes;
-	unsigned int	index;
+| cmd_1       | -options    | arg_1        | arg_2 | NULL  |
++-------------+-------------+--------------+-------+-------+-------+
+| cmd_2       | arg_1       | arg_2        | ...   | arg_n | NULL  |
++-------------+-------------+--------------+-------+-------+-------+
+| cmd_n       | -options    | NULL         |
++-------------+-------------+--------------+-------+
+| In: default | Out: file_1 | Err: default | NULL  |
++-------------+-------------+--------------+-------+
+| NULL        |
++-------------+
 
-	pipes = 0;
-	tokens_len = 0;
-	while (tokens[tokens_len])
-	{
-		if (tokens[tokens_len][0] == PIPE)
-			pipes++;
-		tokens_len++;
-	}
-	cmds_table = malloc((pipes + 1) * sizeof(*cmds_table));
-	if (!cmds_table)
+TODO: Cases to define
+ - If in the middle of pipeline is a redirection?? -> ls >file1 | grep o
+ - where go "&&" on the table
+*/
+char	***parser(char **tokens)
+{
+	char			***table;
+	unsigned int	rows;
+
+	rows = get_table_rows(tokens);
+	printf("PARSER cmds_table rows: %u\n", rows);
+	table = ft_calloc(rows + 2, sizeof(*table));
+	if (!table)
 		return (NULL);
-	index = 0;
-	cmds_table[index] = ft_strdup(tokens[tokens_len]);
-	while(index <= pipes)
+	fill_table_cmds(table, tokens, rows);
+	//fill_table_io(table, tokens, rows);
+	int	row = 0;
+	int	column = 0;
+	while (table[row])
 	{
-		while(tokens_len)
+		while (table[row][column])
 		{
-			if (tokens[tokens_len][0] == PIPE)
-				break;
-			cmds_table[index] = ft_strjoin(cmds_table[index], tokens[tokens_len]);
-			tokens_len--;
+			printf("PARSER table[%i][%i]: %s\n", row, column, table[row][column]);
+			column++;
 		}
-		index++;
+		column = 0;
+		row++;
 	}
-	return (cmds_table);
+	return (table);
 }
